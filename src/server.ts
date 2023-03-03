84f488670bd19c8158a75577def96eafaaec148f
nbc7 <nathanborgescastro@gmail.com>
2023-03-03 15:31:41 -0300

    updated port for production

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-  port: 3333,
+  port: process.env.PORT ? Number(process.env.PORT) : 8080,
   host: '0.0.0.0'
 }).then(()=>{
   console.log('HTTP server running!')
