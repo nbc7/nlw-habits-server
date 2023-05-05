10fa1b438c89c8eea99bac86ccb55f3ed9483016
nbc7 <nathanborgescastro@gmail.com>
2023-05-05 15:54:23 -0300

    added user profile route (/users/:username/profile)

--- src/routes.ts
+++ src/routes.ts
@@ -317,4 +317,25 @@ export async function appRoutes(app: FastifyInstance) {
 
     return { userToken };
   });
+
+  app.post('/users/:username/profile', async (req) => {
+    const getUsernameBody = z.object({
+      username: z.string(),
+    });
+    const { username } = getUsernameBody.parse(req.body);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: `${username}@gmail.com`,
+      },
+    });
+
+    if (!user) return null;
+
+    return {
+      name: user.name,
+      email: user.email,
+      avatarUrl: user.avatarUrl,
+    };
+  });
 }
